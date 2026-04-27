1715c55f197e6e08b059cdef3e077dcbd0edebe3
rvision <nrogic@paycor.com>
2026-04-27 10:06:53 +0000

    Refactor URL configuration to streamline imports and enhance API response structure

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -1,48 +1,37 @@
-"""octofit_tracker URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
+# urls.py for Octofit Tracker
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from . import views
+from rest_framework import routers
+from .views import UserViewSet, TeamViewSet, ActivityViewSet, WorkoutViewSet, LeaderboardViewSet
 
-router = DefaultRouter()
-router.register(r'users', views.UserViewSet, basename='user')
-router.register(r'teams', views.TeamViewSet, basename='team')
-router.register(r'activities', views.ActivityViewSet, basename='activity')
-router.register(r'leaderboard', views.LeaderboardViewSet, basename='leaderboard')
-router.register(r'workouts', views.WorkoutViewSet, basename='workout')
-
-from django.http import JsonResponse
-from django.urls import reverse
+import os
+from rest_framework.response import Response
 from rest_framework.decorators import api_view
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'teams', TeamViewSet)
+router.register(r'activities', ActivityViewSet)
+router.register(r'workouts', WorkoutViewSet)
+router.register(r'leaderboard', LeaderboardViewSet)
+
+
+codespace_name = os.environ.get('CODESPACE_NAME')
+if codespace_name:
+    base_url = f"https://{codespace_name}-8000.app.github.dev"
+else:
+    base_url = "http://localhost:8000"
 
 @api_view(['GET'])
 def api_root(request, format=None):
-    base_url = request.build_absolute_uri('/')
-    api_url = base_url + 'api/'
-    return JsonResponse({
-        'users': api_url + 'users/',
-        'teams': api_url + 'teams/',
-        'activities': api_url + 'activities/',
-        'leaderboard': api_url + 'leaderboard/',
-        'workouts': api_url + 'workouts/',
+    return Response({
+        'users': f'{base_url}/api/users/',
+        'teams': f'{base_url}/api/teams/',
+        'activities': f'{base_url}/api/activities/',
+        'workouts': f'{base_url}/api/workouts/',
+        'leaderboard': f'{base_url}/api/leaderboard/',
     })
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', api_root, name='api-root'),
     path('api/', include(router.urls)),
+    path('', api_root, name='api_root'),
 ]
